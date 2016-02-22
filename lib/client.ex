cf3e8a0e2f210a89774da3d57a1c38e5dd80ce51
Adam Guyot <adam@cobenian.com>
2016-02-22 16:33:37 -0500

    correct solution

--- lib/client.ex
+++ lib/client.ex
@@ -2,7 +2,7 @@ defmodule Client do
   
   def send_messages() do
     client = self()
-    pong = spawn(Procprez, :pong, [])
+    pong = spawn(Procprez, :start, [])
     send(pong, {:echo, client, "olleh"})
     send(pong, {:ping, client})
     send(pong, {:query_db, client, "select * from foo"})
--- lib/procprez.ex
+++ lib/procprez.ex
@@ -1,16 +1,20 @@
 defmodule Procprez do
 
-  def pong() do
-    hc_pid = HitCounter.start()
+  def pong(hc_pid) do
     receive do
       {:echo, pid, mesg} -> send(pid, mesg)
                             send(hc_pid, {:add, self()})
-                            pong()
+                            pong(hc_pid)
       {:ping, pid} -> send(pid, "pong")
                       send(hc_pid, {:add, self()})
-                      pong()
+                      pong(hc_pid)
       {_, pid, _}  -> send(pid, "Unauthorized message")
-         pong()
+                      pong(hc_pid)
     end
+  end
+
+  def start() do
+    hc_pid = HitCounter.start()
+    pong(hc_pid) 
   end
 end
